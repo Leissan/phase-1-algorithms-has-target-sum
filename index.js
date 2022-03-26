74f5c835771897b0d2be5ba057c2e21c01147a05
Leissan Khan <leissan.khan@gmail.com>
2022-03-25 20:03:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,67 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(array, target){
+//Iterate through each number in the array
+for (let i = 0; i < array.length; i++) {
+  //for the current numebr, identify complement that adds to the target (comp=target - current numebr)
+  const complement = target - array[i]
+  //iterate through the rest of the array
+  for (let j = i + 1; j < array.length; j++){
+   //check if any numebr is a compliment
+   //if so, return true
+    if (array[j] === complement) return true
+  }
+}
+//if i reach the end of the array, and no 2 numebrs addup to the target sum, return false
+return false
 }
 
-/* 
+
+
+
+/*function hasTargetSum(array, target) {
+  let i = 0;
+  let j = 1;
+  while (j < array.length){
+      let sum = array[i] + array[j]
+      if (sum === target) return true;
+    
+      if (j === array.length-1){
+          i++
+          j=i
+      }
+      j++
+
+  }
+}*/
+
+/*function hasTargetSum(array, target) {
+  for(i = 0; i < array.length; i++) {
+      for(j = i+1; j < array.length; j++) {
+          if (target === array[i] + array[j]) {
+            return true
+          } else {
+            return false
+          }
+      }
+  }
+}
+
+
   Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
-*/
+  Add your pseudocode here:
+  Iterate through each number in the array
+  for the current numebr, identify compliment that adds to the target (comp=target - current numebr)
+  iterate through the rest of the array, and chekc if any numebr is a compliment
+  if so, return true
+
+  if i reach the end of the array, and no 2 numebrs addup to the target sum, return false
+
+  
+*///sum up array with position in index 0 with all the other positions one by 
+//one; if the sum equals target, we are done. If not, take index 1 and do the same,
+// then index 2.
 
 /*
   Add written explanation of your solution here
